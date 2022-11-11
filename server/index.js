86cb6a4daab25540ee797a0e00a34ad995f6510c
AnkitRawat9442 <gehurecruitankitrawat@gmail.com>
2022-11-12 04:44:19 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const mongoose = require("mongoose");
 require("dotenv").config();
 const dbUrl = process.env.LOCAL_DB_CONNECT;
 const authRoute = require("./routes/auth");
-const mongoOpts = { useNewUrlParser: true };
+const mongoOpts = { useNewUrlParser: true , useNewUrlParser : true };
 
+app.use(cors())
 app.use(express.json());
-mongoose.connect(dbUrl, mongoOpts, (err) => {
+
+mongoose.connect("mongodb+srv://morse_coder:morse_coder@atlascluster.28usg8c.mongodb.net/test", mongoOpts, (err) => {
     if (err) console.log(err);
     else console.log("mongodb is connected");
 });
 
 app.use("/api/user", authRoute);
 
-app.listen(4000, () => console.log("server is running"));
+app.listen(4000, () => console.log("server is running 4000"));
